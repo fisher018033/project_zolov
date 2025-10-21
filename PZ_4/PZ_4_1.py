9c3675475c487bd2304d491a14828e4238860f4e
Vladimir Fisher <gl565387@gmail.com>
2025-10-22 00:06:47 +0300

    Update PZ_4_1.py

--- PZ_4/PZ_4_1.py
+++ PZ_4/PZ_4_1.py
@@ -1,1 +1,13 @@
-
+#Даны два целых числа A и B (A < Б). Найти произведение всех целых чисел от A до B включительно.
+try:
+  A = int(input('Введи первое значение: '))
+  B = int(input('Введи второе значение: '))
+  if A >= B:
+    print('A должно быть меньше B')
+  else:
+    k = 1
+    for mult in range(A, B + 1):
+      k *= mult
+    print(k)
+except ValueError:
+  print('Неверно')
