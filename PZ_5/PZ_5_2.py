9a10fdf75a8ebd678b1177496e8f34d38fcc0dfd
fisher018033 <gl565387@gmail.com>
2025-10-22 17:35:38 +0300

    Variant 7

--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -3,16 +3,14 @@
 try:
   bas = int(input('введите число: '))
   ad = int(input('введите число от 0 до 9 для добавления к базовому: '))
-  ad2 = int(input('введите второе число от 0 до 9 для добавления к предыдущему: '))
-  if ad < 0 or ad > 9 or ad2 < 0 or ad2 > 9:
+  if ad < 0 or ad > 9:
     print('ОТ 0 ДО 9')
   else:
     def AddRightDigit(bas, ad):
-      return str(bas) + str(ad)
+      return bas * 10 + ad
     bas = AddRightDigit(bas, ad)
-    def AddAgain(bas, ad2):
-      return str(bas) + str(ad2)
-    bas2 = AddAgain(bas, ad2)
+    ad = int(input('введите второе число от 0 до 9 для добавления к базоваму: '))
+    bas2 = AddRightDigit(bas, ad)
     print(bas)
     print(bas2)
 except ValueError:
