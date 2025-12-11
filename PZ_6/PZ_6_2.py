4c71a2b16e5ee6a5c1bb89e313653e400a5dfd32
fisher018033 <gl565387@gmail.com>
2025-12-11 12:11:46 +0300

    Variant 7

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -5,8 +5,17 @@ try:
   amount = int(input("Enter amount of numbers: "))
   if amount < 3:
     print('cant')
+  else:
     for i in range(amount):
       num = int(input(f"Enter {i+1} number: "))
       numbers.append(num)
+    max = []
+    def mix(numbers):
+      for u in range(1, len(numbers)-1):
+        if numbers[u-1] < numbers[u] > numbers[u+1]:
+          max.append(numbers[u])
+      return max
+    min_max = min(mix(numbers))
+    print(min_max)
 except ValueError:
   print('nah')
