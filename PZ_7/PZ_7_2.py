634c1bf586be295037a01aec981a315af539ae99
fisher018033 <gl565387@gmail.com>
2025-12-12 11:39:15 +0300

    Variant 7

---
+++ PZ_7/PZ_7_2.py
@@ -0,0 +1,16 @@
+try:
+  def extract_last_part_after_dot(full_path):
+      last_dot = full_path.rfind('.')
+      if last_dot != -1 and last_dot != len(full_path) - 1:
+          extension = full_path[last_dot + 1:]
+          for separator in ['?', '#', '/']:
+              separator_pos = extension.find(separator)
+              if separator_pos != -1:
+                  extension = extension[:separator_pos]
+          return extension
+      return ""
+  url = input('введите строку: ')
+  result = extract_last_part_after_dot(url)
+  print(f"Извлечено: '{result}'")
+except Exception as e:
+  print('nah')
