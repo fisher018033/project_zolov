6b666b362a6ccb04576f09b168c351d55cb80f3d
fisher018033 <gl565387@gmail.com>
2025-12-12 11:27:25 +0300

    Variant 7

---
+++ PZ_7/PZ_7_1.py
@@ -0,0 +1,10 @@
+try:
+  line = input("Введите строчку: ")
+  first = line[0]
+  last = line[-1]
+  first_code = ord(line[0])
+  last_code = ord(line[-1])
+  print(f'Первый символ: {first} и его код {first_code}')
+  print(f'Первый символ: {last} и его код {last_code}')
+except Exception as e:
+  print('nah')
