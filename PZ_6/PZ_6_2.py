99834ea74b9ec00181ee0fba60255baddfb8ae23
fisher018033 <gl565387@gmail.com>
2025-12-13 10:43:23 +0300

    Variant 7

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -1,21 +1,28 @@
 #Дан список размера N. Найти минимальный из его локальных максимумов (локальный минимум — это элемент, который меньше любого из своих соседей).
 
+import random
+
 try:
-  numbers = []
-  amount = int(input("Enter amount of numbers: "))
-  if amount < 3:
-    print('cant')
+
+  N = int(input("Введите размер списка: "))
+  numbers = [random.randint(1, 100) for _ in range(N)]
+  print(f"Случайный список: {numbers}")
+  local_maxs = []
+  if N > 1 and numbers[0] > numbers[1]:
+    local_maxs.append(numbers[0])
+  elif N == 1:
+    local_maxs.append(numbers[0])
+  for i in range(1, N - 1):
+    if numbers[i - 1] < numbers[i] > numbers[i + 1]:
+      local_maxs.append(numbers[i])
+  if N > 1 and numbers[-1] > numbers[-2]:
+    local_maxs.append(numbers[-1])
+  print(f"Локальные максимумы: {local_maxs}")
+  if local_maxs:
+    min_local_max = min(local_maxs)
+    print(f"Минимальный из локальных максимумов: {min_local_max}")
   else:
-    for i in range(amount):
-      num = int(input(f"Enter {i+1} number: "))
-      numbers.append(num)
-    max = []
-    def mix(numbers):
-      for u in range(1, len(numbers)-1):
-        if numbers[u-1] < numbers[u] > numbers[u+1]:
-          max.append(numbers[u])
-      return max
-    min_max = min(mix(numbers))
-    print(min_max)
+    print("Локальные максимумы не найдены")
+
 except ValueError:
-  print('nah')
+  print('Ошибка ввода. Введите целое число.')
--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,21 +1,17 @@
 #Дан список размера N. Возвести в квадрат все его локальные минимумы (то есть числа, меньшие своих соседей).
 
+import random
 try:
-  numbers = []
-  amount = int(input("Enter amount of numbers: "))
-  if amount < 3:
-    print('cant')
-  else:
-    for i in range(amount):
-      num = int(input(f"Enter {i+1} number: "))
-      numbers.append(num)
-    def mix(lst):
-      result = lst.copy()
-      for i in range(1, len(lst) - 1):
-        if lst[i] < lst[i-1] and lst[i] < lst[i+1]:
-          result[i] = lst[i] ** 2
-      return result
-    print(f"Изначальный список: {numbers}")
-    print(f"Возведенные в квадрат локальные минимумы: {mix(numbers)}")
+  N = int(input("Введите размер списка: "))
+  numbers = [random.randint(1, 100) for _ in range(N)]
+  print(f"Случайный список: {numbers}")
+  def mix(lst):
+    result = lst.copy()
+    for i in range(1, len(lst) - 1):
+      if lst[i] < lst[i-1] and lst[i] < lst[i+1]:
+        result[i] = lst[i] ** 2
+    return result
+  print(f"Изначальный список: {numbers}")
+  print(f"Возведенные в квадрат локальные минимумы: {mix(numbers)}")
 except ValueError:
   print('nah')
