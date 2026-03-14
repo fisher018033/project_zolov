3b59f4614bc8d68afe1f42a4a1ffe9ab14ddbfac
fisher <gl565387@gmail.com>
2026-03-14 13:42:39 +0300

    Update PZ_8_2.py

--- PZ_8/PZ_8_2.py
+++ PZ_8/PZ_8_2.py
@@ -5,23 +5,27 @@ s = 'груши 45 991 63 100 12 морковь 13 47 26 0 16'
 print('Исходная строка:', s)
 print()
 
-d = s.split()
+try:
+    
+    d = s.split()
 
-res = {}
-prod = None
-ch = []
+    res = {}
+    prod = None
+    ch = []
 
-for i in d:
-    if i.isdigit():
-        ch.append(int(i))
-    else:
-        if prod:
-            res[prod] = ch
-        prod = i
-        ch = []
-if prod:
-    res[prod] = ch
+    for i in d:
+        if i.isdigit():
+            ch.append(int(i))
+        else:
+            if prod:
+                res[prod] = ch
+            prod = i
+            ch = []
+    if prod:
+        res[prod] = ch
 
-print('Результат:')
-for k, v in res.items():
-    print(f'{k}: {min(v)} кг')
+    print('Результат:')
+    for k, v in res.items():
+        print(f'{k}: {min(v)} кг')
+except ValueError:
+    print("Ошибка")
